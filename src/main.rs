61bb0f529de88c8d5aa2231eb7bdf7eadd970b68
Jacob Stephenson <jacobstephenson97@gmail.com>
2023-02-27 15:41:28 -0500

    Followed Textbook Example for a webserver!

---
+++ src/main.rs
@@ -0,0 +1,60 @@
+use hello::ThreadPool;
+use std::{
+    fs,
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
+
+fn main() {
+    let listener = match TcpListener::bind("127.0.0.1:7878") {
+        Ok(listener) => listener,
+        Err(e) => {
+            println!("Failed to bind to address: {}", e);
+            return;
+        }
+    };
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming().take(2) {
+        let stream = match stream {
+            Ok(stream) => stream,
+            Err(e) => {
+                println!("Failed to establish a connection: {}", e);
+                continue;
+            }
+        };
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+    }
+    println!("Shutting down.");
+}
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, contents) = match &request_line[..] {
+        "GET / HTTP/1.1" => (
+            "HTTP/1.1 200 OK",
+            fs::read_to_string("./src/hello.html").unwrap(),
+        ),
+        "GET /sleep HTTP/1.1" => {
+            std::thread::sleep(std::time::Duration::from_secs(5));
+            (
+                "HTTP/1.1 200 OK",
+                fs::read_to_string("./src/hello.html").unwrap(),
+            )
+        }
+        _ => (
+            "HTTP/1.1 404 NOT FOUND",
+            fs::read_to_string("./src/404.html").unwrap(),
+        ),
+    };
+
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
